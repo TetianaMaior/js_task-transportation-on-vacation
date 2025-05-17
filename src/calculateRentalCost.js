00e6a879ffbf688dd340ffc9359835dc0fcdd850
Tetiana Maior <Solomaho@ukr.net>
2025-05-17 20:22:06 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,14 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let total = days * 40;
+  if(days >= 7 ) {
+    total -= 50;
+  }
+  if(days >= 3 && days <= 6) {
+    total -= 20;
+  }
+  return total;
 }
 
 module.exports = calculateRentalCost;
