51367b5d3bb008e6b1fe3ca56854dbddcce5a7cc
Tetiana Maior <Solomaho@ukr.net>
2025-05-19 13:41:17 +0300

    Lesson

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,15 @@
  */
 function calculateRentalCost(days) {
   let total = days * 40;
-  if(days >= 7 ) {
+
+  if (days >= 7) {
     total -= 50;
   }
-  if(days >= 3 && days <= 6) {
+
+  if (days >= 3 && days <= 6) {
     total -= 20;
   }
+
   return total;
 }
 
